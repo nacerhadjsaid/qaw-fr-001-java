670500280acae78bb1e3e8b0c512f46f23ddb451
nacerhadjsaid <nacer.hadjsaid@gmail.com>
2022-11-12 15:41:17 -0500

    first commit

---
+++ src/Hello.java
@@ -0,0 +1,43 @@
+class Hello {
+    //this is main method
+    public static void main(String[] args) {
+        //this is print statement
+        System.out.print("Hello World");
+
+        /*
+        this is line 1
+        this is line 2
+        this line 3
+        */
+
+        //in-line declaration and assignment
+        int x = 1;
+
+        //declaration
+        int y;
+
+        //assignment
+        y = 5;
+
+        //number
+            //whole number
+            byte b = 23;
+            short s = 243;
+            int i = 246412;
+            long l = 7237847987823L;
+
+            //fraction
+            float f = 1.3823452394F;
+            double d = 1.3245245346463456789876;
+
+        //character
+        char c = ' ';
+
+        //boolean
+        boolean boo = true;
+
+        //string
+        String str = "";
+
+    }
+}
